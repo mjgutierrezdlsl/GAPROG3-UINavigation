857dd8154c4a3887a21dffabe678ef0dcd544042
Jolo Gutierrez <mariano.jose.gutierrez.iii@dlsl.edu.ph>
2024-08-21 00:23:31 +0800

    prevents character from moving beyond screen bounds

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -12,6 +12,15 @@ public class PlayerController : MonoBehaviour
     [Header("Movement")]
     [SerializeField] private float _movementSpeed;
     private Vector2 _moveDirection;
+    private Vector2 _screenBounds;
+    private float _objectWidth, _objectHeight;
+
+    private void Start()
+    {
+        _screenBounds = Camera.main.ScreenToWorldPoint(new(Screen.width, Screen.height, Camera.main.transform.position.z));
+        _objectWidth = _spriteRenderer.bounds.size.x / 2;
+        _objectHeight = _spriteRenderer.bounds.size.y / 2;
+    }
 
     private void Update()
     {
@@ -39,5 +48,12 @@ public class PlayerController : MonoBehaviour
         }
 
         transform.Translate(_moveDirection * _movementSpeed * Time.deltaTime);
+    }
+    private void LateUpdate()
+    {
+        var viewPosition = transform.position;
+        viewPosition.x = Mathf.Clamp(viewPosition.x, _screenBounds.x * -1 + _objectWidth, _screenBounds.x - _objectWidth);
+        viewPosition.y = Mathf.Clamp(viewPosition.y, _screenBounds.y * -1 + _objectHeight, _screenBounds.y - _objectHeight);
+        transform.position = viewPosition;
     }
 }
